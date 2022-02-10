32b0118165b3d796a3ad1ec401fe055a241d40fd
Techdaan1 <88482201+Techdaan1@users.noreply.github.com>
2022-02-10 15:34:36 +0100

    Update Chat.js

--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import { View, Platform, KeyboardAvoidingView, StyleSheet } from "react-native";
+
+import * as firebase from "firebase";
 import "firebase/firestore";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
@@ -191,6 +193,24 @@ export default class Chat extends Component {
     }
   }
 
+  renderCustomView(props) {
+    const { currentMessage } = props;
+    if (currentMessage.location) {
+      return (
+        <MapView
+          style={{ width: 150, height: 100, borderRadius: 13, margin: 3 }}
+          region={{
+            latitude: currentMessage.location.latitude,
+            longitude: currentMessage.location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      );
+    }
+    return null;
+  }
+
   render() {
     //Set bgcolor to color selected on start page
     let bgColor = this.props.route.params.bgColor;
@@ -213,6 +233,7 @@ export default class Chat extends Component {
                 name: this.state.name,
               }}
               renderActions={this.renderCustomActions}
+              renderCustomView={this.renderCustomView}
             />
 
             {Platform.OS === "android" ? (
